b20e854deb880ef227c81f254b94af71c83a5b2e
Arthur Fonseca <arthurandrade8713@gmail.com>
2026-04-10 20:53:00 +0100

    atividade de c#

---
+++ lista2_ex11/lista2_ex11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2_ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //menu de opções
+            string opcao;
+            int num1,num2,soma, subtrair;
+            do
+            {
+                Console.WriteLine("1-Somar\n2-Subtrair\n0-Sair");
+                Console.WriteLine("Escolha uma opção: ");
+                opcao = Console.ReadLine();
+                //Console.Clear();
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Indique 1 número: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Indique 1 número: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        soma = num1 + num2;
+                        Console.WriteLine($"Soma de {num1} + {num2} = {soma}");
+                        break;
+                    
+                    case "2":
+                        Console.WriteLine("Indique 1 número: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Indique 1 número: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        subtrair = num1 - num2;
+                        Console.WriteLine($"Subtração de {num1} - {num2} = {subtrair}");
+                        break;
+                    default:
+                        Console.WriteLine("Opção Invalida");
+                        break;
+                }
+
+            } while (opcao != "0");
+            
+        }
+    }
+}
---
+++ lista2_ex9/lista2_ex9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2_ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max, num,min,cont;
+
+            Console.WriteLine("Insira um valor:");
+            num = Convert.ToInt32(Console.ReadLine());
+            min = num;
+            max = num;
+            do
+            {
+
+                Console.WriteLine("Insira m valor:");
+                num = Convert.ToInt32(Console.ReadLine());
+                if(num > max)
+                {
+                    max = num;
+                }
+                if (num < max)
+                {
+                    min = num;
+                }
+                
+            }while(num != 0);
+            Console.WriteLine($"maximo: {max}");
+            Console.WriteLine($"minimo: {min}");  
+        
+        
+        }
+    }
+}
